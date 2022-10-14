d5ac994cf42ff8924d9f999ac240171d9977901b
Julio Barrientos <113930475+Juliocb12543@users.noreply.github.com>
2022-10-14 17:30:45 +0000

    random practices

--- main.py
+++ main.py
@@ -53,15 +53,17 @@ print(f"Your random color is {random_color}")
 ################################################random in python#################################################
 # Random Practice #1
 # Implement the randint() function from the random library that allows you to obtain an integer from 1 to 10, and store that value in a variable called random_number.
-  
-  
+random_number = randint(1,10) 
+print(random_number)
 # Random Practice #2
 # Implement the random() function from the random library to obtain a real number between 0 and 1, and store that value in a variable called random_number.
-  
+random_number = randint(0,1)
+print(random_number)
   
 # Random Practice #3
 # Use the random library's choice() method to get a random item from the list of names below, and store the chosen name in a variable called raffle.
-  
-# names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
-  
+
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+raffle_winner = choice(names)
+print(f"The raffle winner is {raffle_winner}")  
   
